8a98be24458a5b7845da2ae1f47a2e1fdbc57380
Kamil Tarnowski <kam.tarnowski@gmail.com>
2014-12-16 14:00:09 +0100

    after header links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,19 @@ class StaticPagesController < ApplicationController
   def home
   end
 
+  def kontakt
+  end
+
+  def realizacje
+  end
+
+  def metoda_1
+  end
+
+  def metoda_2
+  end
+
+  def metoda_3
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
+  get '/kontakt', to: 'static_pages#kontakt'
+  get '/realizacje', to: 'static_pages#realizacje'
+  get '/metoda_1', to: 'static_pages#metoda_1'
+  get '/metoda_2', to: 'static_pages#metoda_2'
+  get '/metoda_3', to: 'static_pages#metoda_3'
 end
