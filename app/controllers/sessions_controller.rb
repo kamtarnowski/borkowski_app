611446720d45b836c0a2eca834b922eadb0941cc
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-01-05 21:26:37 +0100

    index

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < Devise::SessionsController
+ before_filter :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+   def new
+     super
+   end
+
+  # POST /resource/sign_in
+   def create
+     super
+     if current_user.admin?
+       if
+          cookies[:opin_numb].to_i < Opinion.count
+          flash[:notice] = 'Dodano nową opinię.'
+          cookies.delete(:opin_numb)
+       end
+     end
+   end
+
+  # DELETE /resource/sign_out
+   def destroy
+     if current_user.admin?
+       cookies[:opin_numb] = Opinion.count
+     end
+     super
+   end
+
+   protected
+
+  # You can put the params you want to permit in the empty array.
+   def configure_sign_in_params
+     devise_parameter_sanitizer.for(:sign_in) << :attribute
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions' }
   root 'static_pages#home'
   resources "contacts", only: [:new, :create]
   resources "opinions"
