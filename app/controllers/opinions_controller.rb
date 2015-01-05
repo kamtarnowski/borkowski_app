a4d9526aed83a9b1bc82789c88048b4b64115aad
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-01-05 15:14:18 +0100

    need users show index

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,8 +1,6 @@
 class OpinionsController < ApplicationController
-  before_action :authenticate_user!
-
-  def new
-  end
+  before_action :authenticate_user!, only: [:create, :show]
+  before_action :correct_admin, only: [:destroy, :index]
 
   def create
     @opinion = current_user.opinion.build(opinion_params)
@@ -23,6 +21,19 @@ class OpinionsController < ApplicationController
 
   def opinion_params
     params.require(:opinion).permit(:content)
+  end
+
+  def correct_admin
+    if user_signed_in?
+      if current_user.admin?
+      else
+        redirect_to root_path
+        flash[:warning] = 'Nie masz uprawnień'
+      end
+    else
+      redirect_to root_path
+      flash[:warning] = 'Nie masz uprawnień'
+    end
   end
 
 end
