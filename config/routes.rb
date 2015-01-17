5c629d6c5ab4dcc7c28b2c0fe3764033916ce320
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-01-17 22:25:01 +0100

    home completed

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   resources :business_methods, only: [:new, :create, :update, :destroy, :show]
   resources :home_pages, only: [:edit, :update]
   root 'static_pages#home'
-  delete 'users/:id' => 'users#destroy', :as => :admin_destroy_user
-  get '/users',       to: 'users#index'
-  get '/contact',     to: 'contacts#new'
-  get '/realizacje',  to: 'static_pages#realizacje'
-  get '/metoda_1',    to: 'static_pages#metoda_1'
-  get '/metoda_2',    to: 'static_pages#metoda_2'
-  get '/metoda_3',    to: 'static_pages#metoda_3'
+  delete 'users/:id'  => 'users#destroy', :as => :admin_destroy_user
+  get '/users'        => 'users#index'
+  get '/contact'      => 'contacts#new'
+  get '/realizacje'   => 'static_pages#realizacje'
+  get '/metoda_1'     => 'static_pages#metoda_1'
+  get '/metoda_2'     => 'static_pages#metoda_2'
+  get '/metoda_3'     => 'static_pages#metoda_3'
 
 end
