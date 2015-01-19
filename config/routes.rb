9a7d4c10e1d0184096703c6bc6e81d3a73392dcd
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-01-19 21:45:56 +0100

    Contact model finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
   devise_for :users, controllers: { sessions: 'sessions' }
   resources :contacts, only: [:new, :create]
   resources :opinions
   resources :company_data, only: [:edit, :update]
   resources :business_methods, only: [:new, :create, :update, :destroy, :show]
   resources :home_pages, only: [:edit, :update]
-  root 'static_pages#home'
   delete 'users/:id'  => 'users#destroy', :as => :admin_destroy_user
   get '/users'        => 'users#index'
   get '/contact'      => 'contacts#new'
