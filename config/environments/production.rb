dcf6f311e4f14eb06a294bacf34d1f74285fe675
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-01-19 21:34:20 +0100

    Contact model finished

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { :host => 'http://yoursite.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://powerful-journey-1883.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
