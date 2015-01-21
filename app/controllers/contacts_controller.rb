69a6b40c8ba759c8b343cce7b60cefec6c9c28db
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-01-21 02:56:02 +0100

    Added Carousel model to Home Page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,13 +5,14 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @com_data = CompanyDatum.find(1)
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Dziękujemy za wiadomość. Skontaktujemy się z Tobą najszybciej jak to tylko możliwe!'
-      render 'static_pages/home'
+      flash[:notice] = 'Dziękujemy za wiadomość. Skontaktujemy się z Tobą najszybciej jak to tylko możliwe.'
+      redirect_to root_path
     else
-      flash.now[:error] = 'Nie można wysłać wiadomości.'
+      flash[:notice] = 'Nie można wysłać wiadomości.'
       render :new
     end
   end
