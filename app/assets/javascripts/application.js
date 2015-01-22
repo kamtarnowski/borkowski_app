182b6a769eefb9085f3cadf5fdb5ac8fe5a5cc78
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-01-22 12:37:13 +0100

    geotag

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,4 +37,22 @@ $(document).ready(function(){
         }
     });
     $('.gritter-item').css('font-size', '16px');
+    handler = Gmaps.build('Google');
+    handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+        markers = handler.addMarkers([
+            {
+                "lat": 50.4296549,
+                "lng": 21.2828111,
+                "picture": {
+                    "image": "marker_google_maps.png",
+                    "width":  36,
+                    "height": 36,
+                },
+                "infowindow": "ul. Kościuszki 35, 28-230 Połaniec",
+            }
+        ]);
+        handler.bounds.extendWith(markers);
+        handler.fitMapToBounds();
+        handler.getMap().setZoom(18);
+    });
 });
