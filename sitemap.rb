a6821fe8e4b50392a4fe4a96b55c7a4d2382aa91
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-02-01 12:40:01 +0100

    sitemap robot.txt

--- sitemap.rb
+++ sitemap.rb
@@ -3,7 +3,15 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'http://borkowski-wylewki-tynki.pl'
 SitemapGenerator::Sitemap.create do
-  add '/home', :changefreq => 'daily', :priority => 0.9
-  add '/contact_us', :changefreq => 'weekly'
+  add '/', :changefreq => 'daily', :priority => 0.9
+  add '/business_methods/1-tynki-agregatem', :changefreq => 'weekly'
+  add '/business_methods/2-wylewki-mixokretem', :changefreq => 'weekly'
+  add '/business_methods/3-posadzki-przemyslowe', :changefreq => 'weekly'
+  add '/galleries/1-galeria', :changefreq => 'weekly'
+  add '/contact', :changefreq => 'weekly'
+  add '/users/sign_up', :changefreq => 'weekly'
+  add '/users/sign_in', :changefreq => 'weekly'
+  add '/users/password/new', :changefreq => 'weekly'
+
 end
 SitemapGenerator::Sitemap.ping_search_engines
